ac1fd69a533aed45efb0f00c50c0ecd266afaa0f
Berezin, Dmitrii <dmitrii.berezin@kronos.com>
2019-11-05 19:21:22 +0300

    Add fibonacci method

---
+++ BasicDataStructures/src/main/java/basic/data/structures/Fibonacci.java
@@ -0,0 +1,40 @@
+package basic.data.structures;
+
+public class Fibonacci {
+    public long fibFormula(int n) {
+        double phi = (1 + Math.sqrt(5)) / 2;
+        return Math.round(Math.pow(phi, n)
+                / Math.sqrt(5));
+    }
+
+    public long fastFibonacciDoubling(int n) {
+        long a = 0;
+        long b = 1;
+        int m = 0;
+        for (int bit = Integer.highestOneBit(n); bit != 0; bit >>>= 1) {
+            // Loop invariant: a = F(m), b = F(m+1)
+
+            // Double it
+            long d = a*((b<<1)-a);
+            long e = (a*a)+(b*b);
+            a = d;
+            b = e;
+            m *= 2;
+
+            // Advance by one conditionally
+            if ((n & bit) != 0) {
+                long c = a+b;
+                a = b;
+                b = c;
+                m++;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Fibonacci helper = new Fibonacci();
+        System.out.println(helper.fastFibonacciDoubling(100));
+        System.out.println(helper.fibFormula(100));
+    }
+}
