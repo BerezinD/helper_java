813ab16ec3bb1d1f48f45910e54d2c0816691477
Berezin, Dmitrii <dmitrii.berezin@kronos.com>
2019-12-01 22:28:27 +0300

    Added memory usages example and gradle files

--- Multithreading/src/main/java/runnable/Main.java
+++ Multithreading/src/main/java/runnable/Main.java
@@ -1,5 +1,7 @@
 package runnable;
 
+import org.openjdk.jol.info.ClassLayout;
+
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 public class Main {
@@ -8,6 +10,20 @@ public class Main {
        log.println("The very first bytes of *.class files is 0xCAFEBABE");
        // here link to ORACLE Documentation https://docs.oracle.com/javase/7/docs/technotes/tools/windows/javap.html
        log.println("javap - The Java Class File Disassembler");
-
+       MemoryRelocated newObject = new MemoryRelocated();
+       log.println(ClassLayout.parseInstance(newObject).toPrintable());
     }
+}
+
+/**
+ * from concurrency course
+ * this class is the example of memory usage.
+ */
+class MemoryRelocated {
+
+    // header 32bit - 8 bytes, 64bit - 12 bytes
+    private int firstInt;       // 4 bytes
+    private short secondShort;    // 2 bytes
+    // padding (выравнивание) to x8 bytes - in our case padding = 24-(4+2+12) = 6
+    // total size of new object of this class is 24 bytes.
 }
