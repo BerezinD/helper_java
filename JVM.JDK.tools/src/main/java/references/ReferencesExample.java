6a4c9443a34d6ab7c44917a74446dcebe12ac2af
BerezinD <ghost_6600@mail.ru>
2019-12-18 22:21:13 +0300

    Example with weak reference

--- JVM.JDK.tools/src/main/java/references/ReferencesExample.java
+++ JVM.JDK.tools/src/main/java/references/ReferencesExample.java
@@ -1,7 +1,25 @@
 package references;
 
+import java.lang.ref.WeakReference;
+
+import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
+
 public class ReferencesExample {
     public static void main(String[] args) {
+        playWithRef();
+    }
 
+    /**
+     * This method may print in log one of this (acceptable for HotSpot VM):
+     *      false, false (possible output without param)
+     *      true, false (-Xcomp)
+     *      true, true (without param, or -Xint)
+     */
+    private static void playWithRef() {
+        Object obj = new Object();
+        WeakReference<Object> ref = new WeakReference<>(obj);
+        log.println(ref.get() != null);
+        System.gc();
+        log.println(ref.get() != null);
     }
 }
